0d8c42c05f602f175687ba78b107344a490507c2
lautschr <144703338+lautschr@users.noreply.github.com>
2023-09-28 12:08:12 +0200

    Added design on whole project

--- scripts/index.js
+++ scripts/index.js
@@ -59,7 +59,7 @@ function displayForecast(response) {
         <div class="weather-forecast-daily-temperatures">
           <span class="weather-forecast-temperature-max"> ${Math.round(
             forecastDay.temp.max
-          )}° </span>
+          )}° </span> |
           <span class="weather-forecast-temperature-min"> ${Math.round(
             forecastDay.temp.min
           )}° </span>
